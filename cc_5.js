14de389d15c24c63231eae16fdfd930a9c53ddc8
mallikanicole <malmichael23@gmail.com>
2025-02-04 17:45:42 -0500

    Task 1-Customer Profile

--- cc_5.js
+++ cc_5.js
@@ -1,2 +1,6 @@
-let customer={name:"John Doe",age:35,email:"john.doe@gmail.com"}
-console.log(customer)
+//Task 1-Object Properties
+let customer={name:"John Doe",age:35,email:"john.doe@gmail.com"};//declare customer details
+for(let x in customer){
+    console.log(`${x}:${customer[x]}`);
+}//list each property separately
+
